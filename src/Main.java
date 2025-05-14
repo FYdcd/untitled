2c488a7502bbeecfc330c5133d23ad2bbfbfe11a
241005 <241005@std.hi-joho.ac.jp>
2025-05-14 10:05:29 +0900

    GitHubにアップロードする手順をデモする為にファイルを変更した。V

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("福原　悠利");
 
-        for(int i=1; i<=5; i++){
+        for(int i=2; i<=8; i++){
             System.out.println("i="+i);
         }
     }
