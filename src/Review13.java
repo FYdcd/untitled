c89f086e21a3112a66da8dc1991a8049f0b3f49e
241005 <241005@std.hi-joho.ac.jp>
2025-05-21 10:32:50 +0900

    復習問題を最後の14まで終わらせた。

---
+++ src/Review13.java
@@ -0,0 +1,19 @@
+public class Review13 {
+    public static void main(String[] args){
+        double randomNum = Math.random() * 6;
+        int fortune = (int)randomNum;
+        if(fortune == 1) {
+            System.out.println("大吉");
+        }else if(fortune == 2) {
+            System.out.println("中吉");
+        }else if(fortune == 3){
+            System.out.println("小吉");
+        }else if(fortune == 4){
+            System.out.println("吉");
+        }else if(fortune == 5){
+            System.out.println("凶");
+        }else{
+            System.out.println("大凶");
+        }
+    }
+}
